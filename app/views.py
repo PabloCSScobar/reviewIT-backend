4355f153f7c585985ac31457624e1c1d214fd5e5
Pawel Gajo <gpawel1997@gmail.com>
2021-03-18 22:03:47 +0100

    dodanie widoków dla odpowiedzi i posta

--- app/views.py
+++ app/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from .serializers import CategorySerializer
-from .models import Category
+from .serializers import *
+from .models import *
 from rest_framework import viewsets, status
 
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    
+
+
+class AnswerView(viewsets.ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+class PostDetailView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+
+
+class PostView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostSerializer
+        else:
+            return PostDetailSerializer
+
