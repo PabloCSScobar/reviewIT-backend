92abd3f5c8ecc4825a188c4f03a17235c0012d2c
Pawel Gajo <gpawel1997@gmail.com>
2021-03-18 22:02:48 +0100

    podpięcie modelu profile do generycznego obiektu User

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from app.models import Answer, Category, Post, Profile, AnswerCategory, AnswerCategoryNode
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 admin.site.register(Answer)
 admin.site.register(AnswerCategory)
@@ -8,3 +9,17 @@ admin.site.register(AnswerCategoryNode)
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(Category)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
