454a3dd0048299f55abe31522923a44022052ad1
Pawel Gajo <gpawel1997@gmail.com>
2021-03-20 19:50:21 +0100

    stworzenie testów dla aktualizacji posta

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -169,3 +169,62 @@ class DeletePostTest(APITestCase):
         response = client.delete(f'/api/posts/{10000000}/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+
+
+class UpdatePostTest(APITestCase):
+    def setUp(self):
+        user = User.objects.create(username='test', password='123456')
+        profile = Profile.objects.create(user=user, reputation=100)
+        categories = Category.objects.all()
+        category = Category.objects.create(name="SEO")
+        self.post = Post.objects.create(
+            author=profile,
+            description='Test post description',
+            title='Test post',
+            repo_link='http://www.google.com',
+            page_link='http://www.google.com',
+            has_top_answer=False)
+
+        self.valid_payload = {
+            "author": profile.pk,
+            "description": "Test2 desc",
+            "title": "Test2 post",
+            "repo_link": "http://www.google.com/test",
+            "page_link": "http://www.google.com/test",
+            "has_top_answer": True,
+            "categories": [category.pk]
+        }
+        self.invalid_payload = {
+            "author": profile.pk,
+            "description": "Test2 desc",
+            "title": "Test2 post",
+            "repo_link": "http://www.google.com/test",
+            "page_link": "",
+            "has_top_answer": None,
+            "categories": [category.pk]
+        }
+
+    
+    def test_valid_update_post(self):
+        response = client.put(
+            f'/api/posts/{self.post.pk}/',
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        
+    def test_invalid_update_post(self):
+        response = client.put(
+            f'/api/posts/{self.post.pk}/',
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = client.put(
+            f'/api/posts/{10000000}/',
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
