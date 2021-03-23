8c315d2661879de47fbef2f2fb3a57f46e3dfca5
Pawel Gajo <gpawel1997@gmail.com>
2021-03-23 21:51:37 +0100

    update cors

--- reviewIt/settings.py
+++ reviewIt/settings.py
@@ -142,5 +142,6 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
-    "http://127.0.0.1:4200"
+    "http://127.0.0.1:4200",
+    "https://reviewit-pawelg.netlify.app"
 ]
